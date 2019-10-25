73d307092ee5eafcad98f3959092ae5e928536ee
Brayan Jeshua <brayanjeshuavz@gmail.com>
2019-10-25 00:37:30 -0500

    Updating Database Extructure

--- database/crud.sql
+++ database/crud.sql
@@ -1,22 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 24-10-2019 a las 00:35:46
--- Versión del servidor: 10.1.38-MariaDB
--- Versión de PHP: 7.1.27
+-- Servidor: localhost:8889
+-- Tiempo de generación: 25-10-2019 a las 05:36:45
+-- Versión del servidor: 5.7.23
+-- Versión de PHP: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `crud`
@@ -33,19 +25,24 @@ USE `crud`;
 CREATE TABLE `customers` (
   `id` int(11) NOT NULL,
   `create_date` date NOT NULL,
-  `company_name` varchar(200) NOT NULL,
-  `contact_name` varchar(200) NOT NULL,
-  `phone` varchar(200) NOT NULL,
   `mail` varchar(200) NOT NULL,
-  `observations` varchar(200) NOT NULL
+  `nombre` varchar(200) NOT NULL,
+  `motivo_visita` varchar(200) NOT NULL,
+  `telefono` varchar(200) NOT NULL,
+  `comentarios` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `customers`
 --
 
-INSERT INTO `customers` (`id`, `create_date`, `company_name`, `contact_name`, `phone`, `mail`, `observations`) VALUES
-(34, '2019-10-23', 'Text', 'sadsad', '6564486', 'sadasdas@dsadsa.asd', 'sadasdsa');
+INSERT INTO `customers` (`id`, `create_date`, `mail`, `nombre`, `motivo_visita`, `telefono`, `comentarios`) VALUES
+(35, '2019-10-23', 'test@test.com', 'Pepito', 'Venta', '3054348536', 'lol'),
+(36, '2019-10-23', 'test@test.com', 'Mauricio', 'Alquiler', '3056098292', 'Lorem Impsut2'),
+(37, '2019-10-23', 'test@test.com', 'Peter', 'Venta', '3056098292', 'Lorem Impsut'),
+(38, '2019-10-23', 'test@test.com', 'Maria', 'Alquiler', '3152208654', 'LOLOLOL'),
+(40, '2019-10-23', 'test@test.com', 'Cristobal', 'Compra', '3056098292', 'Lorem Impsut2'),
+(41, '2019-10-23', 'mayorgajuancamilo@gmail.com', 'Simon', 'Alquiler', '3152208654', 'Lorem Impsut');
 
 --
 -- Índices para tablas volcadas
@@ -65,9 +62,4 @@ ALTER TABLE `customers`
 -- AUTO_INCREMENT de la tabla `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
